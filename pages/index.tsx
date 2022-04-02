81ca3e0eddbb1da2ae0e03ea75fe5bd4e0062448
Ferruh <ferruhcihan20@gmail.com>
2022-04-02 04:55:36 +0300

    feat:update Home page structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
 import type { NextPage } from 'next'
 
 const styles = {
-  wrapper: 'flex min-h-screen flex-col items-center justify-center',
+  wrapper: `h-screen w-screen flex flex-col`,
+  main: `h-full w-screen flex-1 z-10`,
+  mapContainer: `flex-1 w-full h-full`,
+  rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
+  rideRequest: `h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll`,
 }
 
 const Home: NextPage = () => {
   return (
     <div className={styles.wrapper}>
-      <h1>Uber Clone</h1>
+      {/* navbar */}
+      <div className={styles.main}>{/* map */}</div>
+      <div className={styles.rideRequestContainer}>
+        <div className={styles.rideRequest}>
+          {/* location selector */}
+          {/* confirm ride */}
+        </div>
+      </div>
     </div>
   )
 }
