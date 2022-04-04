60f986a49209d9f46b53b97b68a4986e02d1bba1
ferruh <ferruhcihan20@gmail.com>
2022-04-04 22:46:25 +0300

    feat:update Map component

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,11 +1,24 @@
+import mapboxgl from 'mapbox-gl'
+import { useEffect } from 'react'
 import React from 'react'
 
+const styles = {
+  wrapper: `flex-1 h-full w-full`,
+}
+
+// mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+
 const Map = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      Map
-    </div>
-  )
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+      center: [28.9, 41.0],
+      zoom: 10,
+    })
+  }, [])
+
+  return <div className={styles.wrapper} id="map" />
 }
 
 export default Map
