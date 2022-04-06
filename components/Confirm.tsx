6e9e69f62f0d0525479df3b161b0cf347ea20a0c
ferruh <ferruhcihan20@gmail.com>
2022-04-06 23:48:32 +0300

    feat:update Confirm comp

--- components/Confirm.tsx
+++ components/Confirm.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
+const styles = {
+  wrapper: `flex-1 h-full flex flex-col justify-between`,
+  rideSelectorContainer: `h-full flex flex-col overflow-scroll`,
+  confirmButtonContainer: ` border-t-2 cursor-pointer z-10`,
+  confirmButton: `bg-black text-white m-4 py-4 text-center text-xl`,
+}
+
 const Confirm = () => {
-  return <div>Confirm</div>
+  const storeTripDetails = async () => {}
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.rideSelectorContainer}>{/* Ride Selector */}</div>
+      <div className={styles.confirmButtonContainer}>
+        <div className={styles.confirmButtonContainer}>
+          <div
+            className={styles.confirmButton}
+            onClick={() => storeTripDetails()}
+          >
+            Confirm UberX
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Confirm
