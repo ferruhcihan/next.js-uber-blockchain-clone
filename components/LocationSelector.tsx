8f913f9f144bebbe2a79a2596796d1ab8cfd4104
ferruh <ferruhcihan20@gmail.com>
2022-04-06 22:53:05 +0300

    feat:update LocationSelector comp

--- components/LocationSelector.tsx
+++ components/LocationSelector.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const styles = {
+  wrapper: `pt-2`,
+  searchHeader: `w-full font-bold text-left flex items-center text-3xl p-4 overflow-hidden`,
+}
 
 const LocationSelector = () => {
-  return <div>LocationSelector</div>
+  const [inFocus, setInFocus] = useState('from')
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.searchHeader}>
+        {inFocus === 'from' ? 'Where can we pick you up?' : 'Where to?'}
+      </div>
+    </div>
+  )
 }
 
 export default LocationSelector
