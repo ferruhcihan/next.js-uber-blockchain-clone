2051c8ada92d0fdf9cb6f8b226b981dfd0cdd819
ferruh <ferruhcihan20@gmail.com>
2022-04-07 23:14:59 +0300

    feat:update RideSelector comp

--- components/RideSelector.tsx
+++ components/RideSelector.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import uberX from '../assets/rides/uberX.png'
+
+const styles = {
+  wrapper: `h-full flex flex-col`,
+  title: `text-gray-500 text-center text-xs py-2 border-b`,
+  carList: `flex flex-col flex-1 overflow-scroll`,
+  car: `flex p-3 m-2 items-center border-2 border-white`,
+  carImage: `h-14`,
+}
+
+const carList: any[] = [
+  {
+    service: 'UberX',
+    iconUrl: uberX,
+    priceMultiplier: 1,
+  },
+]
 
 const RideSelector = () => {
-  return <div>RideSelector</div>
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>Choose a ride, or swipe up for more</div>
+      <div className={styles.carList}>
+        {carList.map((car, index) => (
+          <div className={styles.car} key={index}>
+            <Image
+              src={car.iconUrl}
+              className={styles.carImage}
+              height={50}
+              width={50}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default RideSelector
