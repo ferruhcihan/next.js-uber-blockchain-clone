3d3aec0ae93485d08d5e6c48ffd6304fee0387ff
ferruh <ferruhcihan20@gmail.com>
2022-04-13 20:29:00 +0300

    feat:add context provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { UberProvider } from '../context/uberContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UberProvider>
+      <Component {...pageProps} />
+    </UberProvider>
+  )
 }
 
 export default MyApp
