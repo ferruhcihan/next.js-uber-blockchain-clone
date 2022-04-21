f42c19e323d9c2ab4ba017dfbab21e7aea08b878
ferruh <ferruhcihan20@gmail.com>
2022-04-21 19:28:50 +0300

    feat:update Navbar comp

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import avatar from '../temp/avatar.jpg'
 import { BsPerson } from 'react-icons/bs'
+import { useContext } from 'react'
+import { UberContext } from '../context/uberContext'
 
 const style = {
   wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20`,
@@ -15,8 +17,7 @@ const style = {
 }
 
 const Navbar = () => {
-  const currentAccount:string = "";
-  const currentUser:any = "";
+  const { currentAccount, connectWallet, currentUser } = useContext(UberContext)
 
   return (
     <div className={style.wrapper}>
@@ -42,7 +43,7 @@ const Navbar = () => {
             {currentAccount.slice(0, 6)}...{currentAccount.slice(39)}
           </div>
         ) : (
-          <div className={style.loginButton} onClick={() => null}>
+          <div className={style.loginButton} onClick={() => connectWallet()}>
             <BsPerson />
             <span className={style.loginText}>Log in</span>
           </div>
